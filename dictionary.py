2a2692446d07c27364628db51985866ed8c6fadd
unknown <nirajadhikari0007@gmail.com>
2025-08-23 14:21:24 +0545

    Dictionary file added

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+# A program that creates a dictionary with keys 'name', 'age', 'city' and prints the values.
+dict1={
+    "Name":"Niraj Adhikari",
+    "Age": 25,
+    "City":"Kathmandu"}
+print(dict1)
