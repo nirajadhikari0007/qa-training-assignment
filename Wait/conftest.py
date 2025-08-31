75706b2f46c1b5d4df5360ed4bd059493ca85d48
unknown <nirajadhikari0007@gmail.com>
2025-09-01 01:08:17 +0545

    Track only POM_Assignment folder

--- Wait/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-@pytest.fixture
-def driver():
-    #setup: open chrome browser
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit() #teardown:close browser
